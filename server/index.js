310fbfcdddf7494a07605ff3ac1b8c28c37f0448
Rishabh-020 <rishabh.ag2007@gmail.com>
2026-01-25 22:27:57 +0530

    SetUp to start my project

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working" });
+});
+
+app.listen(5000, () => console.log("Server running on 5000"));
